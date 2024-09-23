e939e59b6c654646fa29c7cfa57e53eb9e0b0233
nitinkumarpals <nitinpal900@gmail.com>
2024-09-23 15:21:57 +0530

    added home endpoint

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,25 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
-
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Welcome to my app!</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Welcome to my app!</h1>
+        <p>This is a courseApp.</p>
+      </body>
+    </html>
+  `);
+});
 async function start() {
   await mongoose.connect(process.env.MONGODB_URI || "");
   console.log("connected to db");
